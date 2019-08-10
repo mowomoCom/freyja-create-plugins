068681651313f9c646f2038649bde7cddc426a23
Pedro Crespo <pedro@mowomo.com>
2019-08-10 13:11:04 +0200

    En progreso

---
+++ assets/plugin/mwm-support.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: {plugin_name}
+ * Plugin URI:  {plugin_uri}
+ * Description: {plugin_description}
+ * Version:     {plugin_version}
+ * Author:      {plugin_author}
+ * Author URI:  {plugin_author_uri}
+ * License:     {plugin_license}
+ * License URI: {plugin_license_uri}
+ * Text Domain: {plugin_text_domain}
+ * Domain Path: /languages
+ */
---
+++ bin/start.js
@@ -0,0 +1,5 @@
+// Funciones del sistema
+const f = require("./../lib/functions");
+
+// Iniciamos el menú de la herramienta
+f.init();
